a709d49ee5b15d8861b3a612f463b1f9c5088db9
Felipe Moura <42254622+felipetempus@users.noreply.github.com>
2020-12-30 18:02:35 -0500

    Create Highest and Lowest.cs

---
+++ Highest and Lowest.cs
@@ -0,0 +1,22 @@
+using System;
+
+// Link: https://www.codewars.com/kata/554b4ac871d6813a03000035/train/csharp
+
+public static class Kata
+{
+  public static string HighAndLow(string numbers)
+  {
+    string[] subs = numbers.Split(' ');
+    int max = 0;
+    int min = 0;
+    foreach (string i in subs){
+      int result = Int32.Parse(i);
+      if (Int32.Parse(i) > max){
+        max = Int32.Parse(i);
+      } else if (Int32.Parse(i) < min){
+        min = Int32.Parse(i);
+      }
+    }
+    return max.ToString() + " " + min.ToString();
+  }
+}
